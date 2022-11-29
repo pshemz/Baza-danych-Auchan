9de6a8ca9eefe3e611e5521515897781808151eb
pszemekyt <56490148+pszemekyt@users.noreply.github.com>
2022-11-29 18:51:16 +0100

    Update alter.sql

--- Sterydziarz/alter.sql
+++ Sterydziarz/alter.sql
@@ -1,11 +1,11 @@
-ALTER TABLE Informacje_o_dostawie
-ADD CONSTRAINT ID_dostawy FOREIGN KEY (ID_dostawy) REFERENCES Dostawy(ID_dostawy);
+ALTER TABLE INFORMACJE_O_DOSTAWIE
+ADD CONSTRAINT ID_dostawy FOREIGN KEY (ID_dostawy) REFERENCES DOSTAWY(ID_dostawy);
 
-ALTER TABLE Informacje_o_dostawie
-ADD CONSTRAINT ID_magazynu FOREIGN KEY (ID_magazynu) REFERENCES Magazyny(ID_magazynu);
+ALTER TABLE INFORMACJE_O_DOSTAWIE
+ADD CONSTRAINT ID_magazynu FOREIGN KEY (ID_magazynu) REFERENCES MAGAZYNY(ID_magazynu);
 
-ALTER TABLE Koszty_utrzymania
-ADD CONSTRAINT ID_miesiaca_zarobkow FOREIGN KEY (ID_miesiaca_zarobkow) REFERENCES Zarobki(ID_miesiaca_zarobkow);
+ALTER TABLE KOSZTY_UTRZYMANIA
+ADD CONSTRAINT ID_miesiaca_zarobkow FOREIGN KEY (ID_miesiaca_zarobkow) REFERENCES ZAROBKI(ID_miesiaca_zarobkow);
 
-ALTER TABLE Zarobki
-ADD CONSTRAINT ID_banku FOREIGN KEY (ID_banku) REFERENCES Finanse(ID_banku);
+ALTER TABLE ZAROBKI
+ADD CONSTRAINT ID_banku FOREIGN KEY (ID_banku) REFERENCES FINANSE(ID_banku);
