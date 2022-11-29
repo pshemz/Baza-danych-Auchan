c90df15d52cf2cfff0a4de2053ce3094581527b2
pszemekyt <56490148+pszemekyt@users.noreply.github.com>
2022-11-29 20:15:39 +0100

    Update create.sql

--- Sterydziarz/create.sql
+++ Sterydziarz/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE PRACOWNICY (
     imie varchar(30) NOT NULL,
     nazwisko varchar(30) NOT NULL,
     pesel varchar(11) NOT NULL,
-    data_wprowadzenia datetime NOT NULL,
+    data_wprowadzenia date NOT NULL,
     magazynier bit NOT NULL,
     ilosc_godzin_tyg int NOT NULL,
     plec varchar(1) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE PRACOWNICY (
 
 CREATE TABLE INFORMACJE_O_DOSTAWIE (
     ID_raportu int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-    data_dostaw datetime NOT NULL,
+    data_dostaw date NOT NULL,
     wydatki_w_zl int NOT NULL,
     ilosc_pojazdow_przewozacych int NOT NULL
 );
@@ -26,7 +26,7 @@ CREATE TABLE KOSZTY_UTRZYMANIA (
 
 CREATE TABLE ZAROBKI (
     ID_miesiaca_zarobkow int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-    miesiac_rok_zarobkow datetime NOT NULL,
+    miesiac_rok_zarobkow date NOT NULL,
     ilosc_sprzedanych_produktow int NOT NULL,
     ilosc_sprzedanych_produktow_online int NOT NULL,
     ilosc_pieniedzy_w_banku int NOT NULL,
