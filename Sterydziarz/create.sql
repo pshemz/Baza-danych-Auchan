3dde4da14d48eba7fded01a63c6fc579b972287a
pszemekyt <56490148+pszemekyt@users.noreply.github.com>
2022-11-29 19:28:15 +0100

    Update create.sql

--- Sterydziarz/create.sql
+++ Sterydziarz/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE PRACOWNICY (
-    ID_pracownika int NOT NULL PRIMARY KEY,
+    ID_pracownika int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     imie varchar(30) NOT NULL,
     nazwisko varchar(30) NOT NULL,
     pesel varchar(11) NOT NULL,
@@ -11,21 +11,21 @@ CREATE TABLE PRACOWNICY (
 );
 
 CREATE TABLE INFORMACJE_O_DOSTAWIE (
-    ID_raportu int NOT NULL PRIMARY KEY,
+    ID_raportu int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     data_dostaw datetime NOT NULL,
     wydatki_w_zl int NOT NULL,
     ilosc_pojazdow_przewozacych int NOT NULL
 );
 
 CREATE TABLE KOSZTY_UTRZYMANIA (
-    ID_wydatkow int NOT NULL PRIMARY KEY,
+    ID_wydatkow int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     koszty_za_wynajem int NOT NULL,
     koszty_napraw int NOT NULL,
     wydatki_roczne int NOT NULL
 );
 
 CREATE TABLE ZAROBKI (
-    ID_miesiaca_zarobkow int NOT NULL PRIMARY KEY,
+    ID_miesiaca_zarobkow int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     miesiac_rok_zarobkow datetime NOT NULL,
     ilosc_sprzedanych_produktow int NOT NULL,
     ilosc_sprzedanych_produktow_online int NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE ZAROBKI (
 );
 
 CREATE TABLE FINANSE (
-    ID_banku int NOT NULL PRIMARY KEY,
+    ID_banku int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     ilosc_pieniedzy_w_banku int NOT NULL,
     przychody_roczne int NOT NULL,
     wydatki_roczne int NOT NULL,
