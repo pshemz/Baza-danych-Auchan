84cb48df114916b57edaf99e506c02fa3748a857
pszemekyt <56490148+pszemekyt@users.noreply.github.com>
2022-11-29 19:27:02 +0100

    Update create.sql

--- Krzysztof/create.sql
+++ Krzysztof/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE DOSTAWCY (
-	ID_dostawcy int NOT NULL PRIMARY KEY,
+	ID_dostawcy int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	imie varchar(16) NOT NULL,
 	wiek int NULL,
 	zaladunek int NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE DOSTAWCY (
 );
 
 CREATE TABLE DOSTAWY (
-	ID_dostawy int NOT NULL PRIMARY KEY,
+	ID_dostawy int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	ID_zamowienia int NOT NULL,
 	wartosc_dostawy int NOT NULL,
 	data_dostawy date NOT NULL,
@@ -15,14 +15,14 @@ CREATE TABLE DOSTAWY (
 );
 
 CREATE TABLE SPRZEDAZ (
-	ID_sprzedazy int NOT NULL PRIMARY KEY,
+	ID_sprzedazy int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	sprzedaz_w_mies int NOT NULL,
 	zarobki	int NULL,
 	nr_sprzedazy int NOT NULL
 );
 
 CREATE TABLE PRZYCHOD_MIESIECZNY (
-	ID_przychodu int NOT NULL PRIMARY KEY,
+	ID_przychodu int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	kwota_zakupu int NOT NULL,
 	zarobki int NULL,
 	cena_netto int NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE PRZYCHOD_MIESIECZNY (
 );
 
 CREATE TABLE DZIAL ( 
-	ID_dzialu int NOT NULL PRIMARY KEY,
+	ID_dzialu int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	nazwa_dzialu int NOT NULL,
 	przeznaczenie varchar(50) NOT NULL,
 	dzial_sklepowy bool NOT NULL,
