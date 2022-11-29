daeac08085063464842ca10b3a2a7ed0cc0245aa
Przemek <przemek.lukasiak07@gmail.com>
2022-11-29 17:58:32 +0100

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,36 @@
+CREATE TABLE ADRES (
+    ID_adresu int NOT NULL PRIMARY KEY,
+    miasto varchar(16) NOT NULL,
+    wojewodztwo varchar(16) NULL,
+    ulica varchar(16) NOT NULL,
+    numer_budynku int NOT NULL
+);
+
+CREATE TABLE DZIAŁKA (
+    ID_dzialki int NOT NULL PRIMARY KEY,
+    powierzchnia varchar(16) NOT NULL,
+    szerokosc varchar(16) NULL,
+    dlugosc varchar(16) NULL
+);
+
+CREATE TABLE LOKALIZACJA (
+    ID_lokalizacji int NOT NULL PRIMARY KEY,
+    odleglosc_od_centrum int NOT NULL,
+    sklep_w_miescie bool NULL,
+    strefa_ekonomiczna bool NOT NULL
+);
+
+CREATE TABLE PARKING (
+    ID_parkingu int NOT NULL PRIMARY KEY,
+    ilosc_miejsc int NOT NULL,
+	ilosc_miejsc int NOT NULL,
+    parking_bezplatny bool NULL,
+    firma_parkingowa varchar(16) NOT NULL
+);
+
+CREATE TABLE DOSTAWCZAKI (
+    ID_dostawczaka int NOT NULL PRIMARY KEY,
+    marka varchar(16) NOT NULL,
+    kolor varchar(16) NULL,
+    rocznik int NOT NULL,
+);
